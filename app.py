06e66b5601d1442a54e1d2f75d4d7236ade00915
ShawenHarker <harkershawen@gmail.com>
2024-10-18 14:12:33 +0200

    basic home route completed

--- app.py
+++ app.py
@@ -1,3 +1,1 @@
 from tina4_python import *
-
-# 2 routes 1 for the website and one for the PDF
--- src/routes/api.py
+++ src/routes/api.py
@@ -1,2 +1,21 @@
-from tina4_python import *
+from tina4_python.Router import get
+from tina4_python.Template import Template
 
+@get('/')
+async def home(request, response):
+    try:
+        html = Template.render_twig_template('index.twig')
+
+        return response(html)
+    except Exception as e:
+        return response(f"Error: {str(e)}", status=500)
+
+
+@get('/download')
+async def download_pdf(request, response):
+    try:
+        html = Template.render_twig_template('pdf_print.twig')
+
+        return 'Hello World'
+    except Exception as e:
+        return e
