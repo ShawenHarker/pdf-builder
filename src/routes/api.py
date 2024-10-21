1294f98012e2ca00af9af8ffbe25d8e25d624fb1
ShawenHarker <harkershawen@gmail.com>
2024-10-21 14:04:56 +0200

    playwright doesn't generate a pdf with css

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,5 +1,8 @@
 from tina4_python.Router import get
 from tina4_python.Template import Template
+from playwright.async_api import async_playwright
+from tina4_python.Response import Response
+from tina4_python import Constant
 
 @get('/')
 async def home(request, response):
@@ -11,11 +14,32 @@ async def home(request, response):
         return response(f"Error: {str(e)}", status=500)
 
 
-@get('/download')
+#playwright is more up to date than pyppeteer
+async def generate_pdf_from_html():
+    async with async_playwright() as p:
+        browse = await p.chromium.launch()
+        page = await browse.new_page()
+
+        html = Template.render_twig_template('pdf_print.twig')
+        pdf_path = 'results.pdf'
+
+        await page.set_content(html)
+
+        await page.pdf(path=pdf_path, format='A4')
+
+        await browse.close()
+
+
+@get('/download_pdf')
 async def download_pdf(request, response):
     try:
-        html = Template.render_twig_template('pdf_print.twig')
-
-        return 'Hello World'
+        # Step 1 await generate_pdf_from_html()
+        await generate_pdf_from_html()
+        # Step 2 Get the file content
+        content = open('results.pdf')
+        # Step 3 Set the headers to content-type to application/pdf.
+        content_type = Constant.APPLICATION_PDF
+        # Step 4 Return the content in the response call response from the tina4 library
+        return Response(content, content_type=content_type)
     except Exception as e:
-        return e
+        return response(f"Error: {str(e)}")
